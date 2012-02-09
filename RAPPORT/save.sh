0f828a8c171e4af70c63f8997a384ab17e54ce5f
evollis-6 <evollis-6@evollis6-VirtualBox.(none)>
2012-02-09 19:00:10 +0100

    .

--- RAPPORT/save.sh
+++ RAPPORT/save.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-DATE=`date | tr -s ' ' '_' | tr -d ','`
-cp rapport.pdf ~/Dropbox/MongoDB/Rapport/rapport.${DATE}.pdf
+if [ -f ./"rapport.pdf" ]
+then
+    DATE=`date | tr -s ' ' '_' | tr -d ','`
+    cp rapport.pdf ~/Dropbox/MongoDB/Rapport/rapport.${DATE}.pdf
+    rm -i rapport.pdf
+else
+    echo "file rapport.pdf missing"
+fi 

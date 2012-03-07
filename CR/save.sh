fceffc524460cda41798309d20ba83dca23f9694
lbarro <lbarro@ubuntu.ubuntu-domain>
2012-03-07 10:19:02 +0100

    commit

--- CR/save.sh
+++ CR/save.sh
@@ -2,7 +2,8 @@
 FILE="CR"
 if [ -f ./${FILE}.pdf ]
 then
-    DATE=`date | tr -s ' ' '_' | tr -d ','`
+    #DATE=`date | tr -s ' ' '_' | tr -d ','`
+    DATE=01.00.00
     cp ${FILE}.pdf ~/Dropbox/MongoDB/DocSortantsValides/${FILE}.${DATE}.pdf
     rm -i ${FILE}.pdf
 else

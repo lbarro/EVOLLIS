a99ae4350f4d0c5dcb227b20ea5c31551230d5be
lbarro <lbarro@ubuntu.ubuntu-domain>
2012-03-12 16:56:13 +0100

    commit

--- RAPPORT/save.sh
+++ RAPPORT/save.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -f ./"rapport.pdf" ]
 then
-    DATE=`date | tr -s ' ' '_' | tr -d ','`
+    DATE=10_00_00 #date | tr -s ' ' '_' | tr -d ','`
     cp rapport.pdf ~/Dropbox/MongoDB/Rapport/rapport.${DATE}.pdf
     rm -i rapport.pdf
 else
